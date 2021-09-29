05c7972d167775742ff1c8b0cffdb93acd6c72f5
kazuma1989 <kazuma.ebina@quipper.com>
2021-09-29 16:31:00 +0900

    chore: Upgrade packages
    @vitejs/plugin-react-refresh -> @vitejs/plugin-react

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,14 @@ declare const process: {
   }
 }
 
-import reactRefresh from "@vitejs/plugin-react-refresh"
+import react from "@vitejs/plugin-react"
 import { UserConfig, UserConfigFn } from "vite"
 
 const { BROWSER, HOST, PORT, BUILD_PATH } = process.env
 
-const config: UserConfigFn = async () => {
+const config: UserConfigFn = () => {
   const config: UserConfig = {
-    plugins: [
-      // "Fast Refresh" を有効にする。
-      // 書き換えた React モジュールだけがブラウザーに再読み込みされるようになる。
-      reactRefresh(),
-    ],
-
-    esbuild: {
-      // react-jsx 形式の JSX 変換が扱えないビルドツールで擬似的に react-jsx 形式を実現するためのハック。
-      // 擬似的というのは、import React をしないでいいというソースコード観点しか満たせず、JSX 変換後の動作効率の点は満たせないということ。
-      jsxFactory: "_implicit_React.createElement",
-      jsxFragment: "_implicit_React.Fragment",
-      jsxInject: 'import _implicit_React from "react"',
-    },
+    plugins: [react()],
 
     server: {
       // localhost 以外で起動したい場合は指定する。
