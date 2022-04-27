1799e120bed103ad55a9f167a3becc7cd457652c
kazuma1989 <kazuma.ebina@quipper.com>
2022-04-27 21:16:56 +0900

    @emotion/css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { css } from "@emotion/css"
+
+const COLOR = "blue"
+
 export function App() {
   return (
-    <div>
+    <div
+      className={css`
+        color: ${COLOR};
+      `}
+    >
       <h1>Hello</h1>
     </div>
   )
