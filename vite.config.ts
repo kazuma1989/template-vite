9024c2670ca0158b34c68bf551545c963b2baada
kazuma1989 <kazuma.ebina@quipper.com>
2022-04-27 20:57:03 +0900

    refactor: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-declare const process: {
-  readonly env: {
-    readonly [name: string]: string
-  }
-}
-
 import react from "@vitejs/plugin-react"
-import { UserConfig, UserConfigFn } from "vite"
+import { defineConfig, Plugin } from "vite"
 
-const { BROWSER, HOST, PORT, BUILD_PATH } = process.env
+export default defineConfig(async ({ command, mode }) => {
+  const { BROWSER, BUILD_PATH, HOST, PORT, PREVIEW_PORT } = process.env
 
-const config: UserConfigFn = () => {
-  const config: UserConfig = {
-    plugins: [react()],
+  let checkerPlugin: Plugin | undefined
+  if (command === "serve" && mode !== "production") {
+    const checker = await import("vite-plugin-checker").then((m) => m.default)
 
+    checkerPlugin = checker({
+      typescript: true,
+    })
+  }
+
+  return {
     server: {
       // localhost 以外で起動したい場合は指定する。
       host: HOST || "localhost",
@@ -33,9 +34,18 @@ const config: UserConfigFn = () => {
       // デバッグのためソースマップを有効にしておく。
       sourcemap: true,
     },
+
+    preview: {
+      // ポートが衝突したら自動でインクリメントしてくれるので問題ない。
+      port: (PREVIEW_PORT && parseInt(PREVIEW_PORT)) || 3000,
+    },
+
+    plugins: [
+      // The all-in-one Vite plugin for React projects.
+      react(),
+
+      // type-check
+      checkerPlugin,
+    ],
   }
-
-  return config
-}
-
-export default config
+})
